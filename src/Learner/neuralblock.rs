a014893ad884f209305782e608fe3bcc219d074a
Stephen Ashmore <StephenCAshmore@gmail.com>
2017-01-19 23:32:50 -0600

    Added several new files, ran cargo fmt but it did not do much, revist this or fix linter and stylle errors.

---
+++ src/Learner/neuralblock.rs
@@ -0,0 +1,17 @@
+use Tensor::Rank2Tensor;
+use Tensor::Rank1Tensor;
+use Tensor::Vector;
+
+pub struct NeuralBlock {
+    m_weights: Vector<Rank2Tensor>,
+    m_bias: Vector<Rank1Tensor>,
+}
+
+impl NeuralBlock {
+
+
+    pub fn test() -> bool {
+
+        true
+    }
+}
